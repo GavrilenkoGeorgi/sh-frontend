0b7a4fcd7b0738f43d799fbd539e13ebae4873e5
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-09-06 10:54:40 +0300

    Fix missing type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
+import { ReactNode } from 'react'
+
 export type post = {
   _id: string,
   title: string,
   pubDate?: string,
   link: string
+}
+
+export type AuthProviderProps = {
+  children: ReactNode
 }
