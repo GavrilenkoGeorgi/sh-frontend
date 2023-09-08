7e58e52460196134a9f3b80c48af74e157b5883a
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-09-08 17:15:57 +0300

    Add 'register' route

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,6 +7,7 @@ import { useLoginMutation } from '../store/slices/userApiSlice'
 
 import LoadingNotification from '../components/LoadingNotification'
 import styles from './Login.module.sass'
+import formStyles from './Form.module.sass'
 
 const Login: FC = () => {
 
@@ -37,7 +38,7 @@ const Login: FC = () => {
     <form
       id='login'
       onSubmit={(event) => { void handleSubmit(event) }}
-      className={styles.form}
+      className={formStyles.form}
     >
       <input
         type='email'
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,13 +10,15 @@ import MainPage from '../pages/Main'
 import LoginPage from '../pages/Login'
 import ProfilePage from '../pages/Profile'
 import LogoutPage from '../components/Logout'
-import ProtectedRoute from './/ProtectedRoute'
 import GlobalStatsPage from '../pages/GlobalStats'
+import RegisterPage from '../pages/Register'
+import ProtectedRoute from './ProtectedRoute'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<MainPage />} />
+      <Route path='/register' element={<RegisterPage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/logout' element={<LogoutPage />} />
       <Route path='' element={<ProtectedRoute />}>
