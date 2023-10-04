c20d22719797cac8dc2de79ac40b8f7d4311a0e5
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-10-04 18:18:08 +0300

    Add 'dice' component

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,8 +1,18 @@
 import React, { type FC } from 'react'
 
+import Dice from '../components/Dice'
+import styles from './Game.module.sass'
+
 const GamePage: FC = () => {
-  return <section>
-    <h1>Game</h1>
+  return <section className={styles.game}>
+    <div className={styles.dice}>
+      <Dice kind={1} />
+      <Dice kind={2} />
+      <Dice kind={3} />
+      <Dice kind={4} />
+      <Dice kind={5} />
+      <Dice kind={6} />
+    </div>
   </section>
 }
 
