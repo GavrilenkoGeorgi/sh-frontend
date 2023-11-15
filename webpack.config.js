7286223c8d33afbb6de8b5f72b8b9e0f93318083
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-11-15 11:06:10 +0200

    Add PWA icons

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
+      // and not allow any straggling 'old' SWs to hang around
       clientsClaim: true,
       skipWaiting: true,
       maximumFileSizeToCacheInBytes: 5000000
@@ -61,26 +61,24 @@ module.exports = {
       name: 'Sh dice game',
       publicPath: '/',
       fingerprints: true,
+      id: '/',
+      display: 'standalone',
       short_name: 'Sh',
       filename: 'manifest.json',
       description: 'Dice game',
-      background_color: '#ffffff',
-      crossorigin: 'use-credentials' // can be null, use-credentials or anonymous
-      /* icons: [
+      theme_color: '#7B1FA2',
+      background_color: '#7B1FA2',
+      crossorigin: 'use-credentials', // can be null, use-credentials or anonymous
+      icons: [
         {
-          src: path.resolve('src/assets/icon.png'),
-          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          src: path.resolve('src/assets/icons/android-chrome-192x192.png'),
+          size: '192x192' // you can also use the specifications pattern
         },
         {
-          src: path.resolve('src/assets/large-icon.png'),
-          size: '1024x1024' // you can also use the specifications pattern
-        },
-        {
-          src: path.resolve('src/assets/maskable-icon.png'),
-          size: '1024x1024',
-          purpose: 'maskable'
+          src: path.resolve('src/assets/icons/android-chrome-512x512.png'),
+          size: '512x512'
         }
-      ] */
+      ]
     })
   ],
   devServer: {
