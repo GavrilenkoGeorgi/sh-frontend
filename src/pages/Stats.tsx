4b81485d1bfa881aa230c2a6a6b0970a18a6775c
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-11-28 13:02:23 +0200

    Update stats page
    - Count up animation for score stats
    - Chart tooltips now show timestamp and value

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, type FC, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import CountUp from 'react-countup'
 import { useGetStatsMutation } from '../store/slices/gameApiSlice'
 import { setNotification } from '../store/slices/notificationSlice'
 import { type iStats } from '../types'
+import { formatChartAxisData } from '../utils'
 import AreaChart from '../components/charts/AreaChart'
 import BarChart from '../components/charts/BarChart'
 import VertBarChart from '../components/charts/VertBarChart'
@@ -17,7 +19,11 @@ const StatsPage: FC = () => {
   const loadStats = async (): Promise<void> => {
     try {
       const data = await getStats({}).unwrap()
-      setStats(data)
+      setStats({
+        ...data,
+        scores: formatChartAxisData(data.scores),
+        schoolScores: formatChartAxisData(data.schoolScores)
+      })
     } catch (err: any) {
       dispatch(setNotification({ msg: err.error, type: 'error' }))
     }
@@ -27,14 +33,37 @@ const StatsPage: FC = () => {
     void loadStats()
   }, [])
 
-  return <section className={styles.container}>
-    <h1>Stats</h1>
-    <h2>Highest score: <span>{stats?.max}</span></h2>
-    <h3>Average: <span>{stats?.average}</span> which
-      is <span>{stats?.percentFromMax}%</span> from max
-    </h3>
-    <h4>{stats?.games} games</h4>
-    {stats != null && <>
+  return <>
+    {stats != null && <section className={styles.container}>
+      <h1>Stats</h1>
+      <h2>Highest score: <span>
+        <CountUp
+          start={0}
+          end={stats.max}
+          duration={4}
+        />
+        </span>
+      </h2>
+      <h3>Average:&nbsp;
+        <span>
+          <CountUp
+            start={0}
+            end={stats.average}
+            delay={1}
+            duration={4}
+          />
+        </span> which is&nbsp;
+        <span>
+          <CountUp
+            start={0}
+            end={stats.percentFromMax}
+            delay={3}
+            duration={4}
+            suffix='%'
+          />
+        </span> from max
+      </h3>
+      <h4>{stats.games} games so far</h4>
       <aside>
         <h4>School scores</h4>
         <div className={styles.hChart}>
@@ -59,9 +88,8 @@ const StatsPage: FC = () => {
           <VertBarChart data={stats.favComb} />
         </div>
       </aside>
-    </>
-    }
-  </section>
+    </section>
+  }</>
 }
 
 export default StatsPage
