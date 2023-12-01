87deaff4c08d4afef9ea68570117d1ebfa75007d
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-12-01 17:33:22 +0200

    Add 'confetti' animation

--- src/components/layout/Modal.tsx
+++ src/components/layout/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { type FC } from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import type { Nullable } from '../../types'
 
 import LoadingIndicator from './LoadingIndicator'
@@ -19,23 +20,35 @@ const Modal: FC<ModalProps> = ({ heading, text, btnLabel, isBusy = false, onClic
 
   return <div className={styles.modal}>
     <div className={styles.blur}></div>
-    <div className={styles.message}>
-      <h2>{heading}</h2>
-      <p>{text} {score}</p>
-        {userName == null && <Link
-          to='/register'
-          className={styles.link}
-          >
-            Register to save results and view stats
-          </Link>
-        }
+    <motion.div
+      key='modal'
+      initial={{ y: -40, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        duration: 4,
+        ease: [0.6, -0.05, 0.01, 0.99],
+        type: 'spring',
+        stiffness: 100
+      }}
+    >
+      <div className={styles.message}>
+        <h2>{heading}</h2>
+        <p>{text} {score}</p>
+          {userName == null && <Link
+            to='/register'
+            className={styles.link}
+            >
+              Register to save results and view stats
+            </Link>
+          }
         <button onClick={onClick} disabled={isBusy}>
           {isBusy
             ? <LoadingIndicator />
             : `${btnLabel}`
           }
         </button>
-    </div>
+      </div>
+    </motion.div>
   </div>
 
 }
