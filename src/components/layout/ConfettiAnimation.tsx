4c5d78f11b1601666d9861ea8f9779783e906d43
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-12-01 19:13:10 +0200

    Update animation settings

--- src/components/layout/ConfettiAnimation.tsx
+++ src/components/layout/ConfettiAnimation.tsx
@@ -6,19 +6,29 @@ const ConfettiAnimation: FC = () => {
   const { runAnimation } = useConfetti({
     images: [
       {
-        src: '/img/confetti-ribbon.svg',
-        size: 66,
-        weight: 5
+        src: '/img/star.svg',
+        size: 24,
+        weight: 2
       },
       {
-        src: '/img/star.svg',
-        size: 20,
-        weight: 3
+        src: '/img/square.svg',
+        size: 10,
+        weight: 4
+      },
+      {
+        src: '/img/circle.svg',
+        size: 12,
+        weight: 2
+      },
+      {
+        src: '/img/confetti-ribbon.svg',
+        size: 60,
+        weight: 5
       }
     ],
     rotate: true,
     particleCount: 50,
-    duration: 3000,
+    duration: 4000,
     fadeOut: 2000
   })
 
