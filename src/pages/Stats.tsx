e365d32732d09a56fa7dd5a66275c9837cdd1043
Gosha <gavrilenko.georgi@gmail.com>
2026-04-02 14:06:05 +0300

    Fix stats page not loading for newly created users

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -6,6 +6,7 @@ import AreaChart from '../components/charts/AreaChart'
 import BarChart from '../components/charts/BarChart'
 import VertBarChart from '../components/charts/VertBarChart'
 import * as styles from './Stats.module.sass'
+import * as sharedStyles from './SharedStyles.module.sass'
 import Fallback from '../components/layout/Fallback'
 
 const StatsPage: FC = () => {
@@ -14,64 +15,74 @@ const StatsPage: FC = () => {
   if (!data || isLoading) return <Fallback />
 
   return (
-    <section className={styles.statsPage}>
+    <section className={sharedStyles.contentPage}>
       <div className={styles.stats}>
         <h1>Stats</h1>
+        {data.games === 0 ? (
+          <h2 className={styles.noGames}>No games played yet</h2>
+        ) : (
+          <>
+            <h2>
+              Highest score:{' '}
+              <span>
+                <CountUp start={0} end={data.max} delay={0.75} duration={3} />
+              </span>
+            </h2>
 
-        <h2>
-          Highest score:{' '}
-          <span>
-            <CountUp start={0} end={data.max} delay={0.75} duration={3} />
-          </span>
-        </h2>
+            <h3>
+              Average:&nbsp;
+              <span>
+                <CountUp
+                  start={0}
+                  end={data.average}
+                  delay={1.25}
+                  duration={3}
+                />
+              </span>{' '}
+              which is&nbsp;
+              <span>
+                <CountUp
+                  start={0}
+                  end={data.percentFromMax}
+                  delay={1.75}
+                  duration={4}
+                  suffix="%"
+                />
+              </span>{' '}
+              from max
+            </h3>
 
-        <h3>
-          Average:&nbsp;
-          <span>
-            <CountUp start={0} end={data.average} delay={1.25} duration={3} />
-          </span>{' '}
-          which is&nbsp;
-          <span>
-            <CountUp
-              start={0}
-              end={data.percentFromMax}
-              delay={1.75}
-              duration={4}
-              suffix="%"
-            />
-          </span>{' '}
-          from max
-        </h3>
+            <h4>{data.games} games so far</h4>
 
-        <h4>{data.games} games so far</h4>
+            <aside>
+              <h4>School scores</h4>
+              <div className={styles.hChart}>
+                <AreaChart data={formatDateChartAxisData(data.schoolScores)} />
+              </div>
+            </aside>
 
-        <aside>
-          <h4>School scores</h4>
-          <div className={styles.hChart}>
-            <AreaChart data={formatDateChartAxisData(data.schoolScores)} />
-          </div>
-        </aside>
+            <aside>
+              <h4>Scores</h4>
+              <div className={styles.hChart}>
+                <AreaChart data={formatDateChartAxisData(data.scores)} />
+              </div>
+            </aside>
 
-        <aside>
-          <h4>Scores</h4>
-          <div className={styles.hChart}>
-            <AreaChart data={formatDateChartAxisData(data.scores)} />
-          </div>
-        </aside>
+            <aside>
+              <h4>Fav dice values</h4>
+              <div className={styles.hChart}>
+                <BarChart data={formatLabelChartAxisData(data.favDiceValues)} />
+              </div>
+            </aside>
 
-        <aside>
-          <h4>Fav dice values</h4>
-          <div className={styles.hChart}>
-            <BarChart data={formatLabelChartAxisData(data.favDiceValues)} />
-          </div>
-        </aside>
-
-        <aside>
-          <h4>Freq combinations</h4>
-          <div className={styles.sChart}>
-            <VertBarChart data={formatLabelChartAxisData(data.favComb)} />
-          </div>
-        </aside>
+            <aside>
+              <h4>Freq combinations</h4>
+              <div className={styles.sChart}>
+                <VertBarChart data={formatLabelChartAxisData(data.favComb)} />
+              </div>
+            </aside>
+          </>
+        )}
       </div>
     </section>
   )
